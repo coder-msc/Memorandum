c926127f196131a10de63f2a630fe1432c1b5db0
scma <59968873@qq.com>
2021-01-16 20:52:52 +0800

    普通冒泡排序，可以做到最好时间复杂度为O(n)

--- 算法数据结构/src/main/java/排序算法/冒泡排序.java
+++ 算法数据结构/src/main/java/排序算法/冒泡排序.java
@@ -1,4 +1,35 @@
 package 排序算法;
 
 public class 冒泡排序 {
+    public static void main(String[] args) {
+        int[] a ={3,4,2,1,-7,0};
+        BubbleSort(a);
+//        maopao2(a);
+        print(  BubbleSort(a));
+
+    }
+
+    public static   void  print(int[] a){
+    for (int i =0;i<a.length;i++) {
+        System.out.print(a[i]+" ");
+    }
+    System.out.println();
+}
+    public static int[]  BubbleSort(int[] arr){
+        int flag=0;
+        for(int i=0;i<arr.length-1 ;i++){
+            for(int j=0;j<arr.length-1-i;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    flag++;
+                }
+            }
+            if(flag==0){
+                break;
+            }
+        }
+        return  arr;
+    }
 }
