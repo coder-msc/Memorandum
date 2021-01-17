2cc597f154a1263b05e3f067133520ff255bcf77
scma <59968873@qq.com>
2021-01-17 19:33:11 +0800

    希尔排序

--- 算法数据结构/src/main/java/排序算法/希尔排序.java
+++ 算法数据结构/src/main/java/排序算法/希尔排序.java
@@ -3,7 +3,8 @@ package 排序算法;
 public class 希尔排序 {
     public static void main(String[] args) {
         int[] a = {3, 4, 2, 1, -7, 0};
-        ShellSort(a);
+//        ShellSort(a);
+        ShellSortMove(a);
         for(int a1:a){
             System.out.print(a1);
 
@@ -13,7 +14,6 @@ public class 希尔排序 {
     }
     public static int[] ShellSort(int[] arr){
         for(int jap=arr.length/2;jap>0;jap/=2){
-
             for (int i=jap;i<arr.length;i++){
                 int x=i;
                 while(x-jap>=0 && arr[x]<arr[x-jap]){
@@ -25,5 +25,21 @@ public class 希尔排序 {
             }
         }
         return  arr;
+    }
+
+    //基于移动的希尔排序 int[] a = {3, 4, 2, 1, -7, 0};
+    public static int[] ShellSortMove(int[] arr){
+        for(int jap=arr.length/2;jap>0;jap/=2){
+            for(int i=jap;i<arr.length;i++){
+                int x=i;
+                int tmp =arr[x];
+                while(x-jap>=0 && tmp<arr[x-jap]){
+                    arr[x] =arr[x-jap];
+                    x=x-jap;
+                }
+                arr[x] =tmp;
+            }
+        }
+        return arr;
     }
 }
