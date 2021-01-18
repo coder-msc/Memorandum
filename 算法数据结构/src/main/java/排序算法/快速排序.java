841ab1b139a0312ced9c8f265b4c684185c49cfd
scma <59968873@qq.com>
2021-01-18 23:58:34 +0800

    快速排序

---
+++ 算法数据结构/src/main/java/排序算法/快速排序.java
@@ -0,0 +1,29 @@
+package 排序算法;
+
+import static 排序算法.快速排序.QuickSort;
+
+public class 快速排序 {
+    public static void main(String[] args) {
+        int[] a = {3, 4, 2, 1, -7, 0};
+        QuickSort(a,0,a.length-1);
+        System.out.print(a);
+    }
+
+    public static void QuickSort(int[] arr,int low,int high){
+        int l=low;
+        int r =high;
+        if(l>r){
+            return;
+        }
+        int tar=arr[l];
+        while(r>l){
+            while(r>l&& arr[r]>tar){r--;}
+            arr[l]=arr[r];
+            while(r>l && arr[l]<tar){l++;}
+            arr[r]=arr[l];
+        }
+        arr[l] =tar;
+        QuickSort(arr,low,l-1);
+        QuickSort(arr,l+1,high);
+    }
+}
