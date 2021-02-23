24dc8ad5f1b1981206581ca96a2a21267c1513e5
scma <59968873@qq.com>
2021-02-23 23:23:10 +0800

    插入稳定 最好是n

--- 算法数据结构/src/main/java/排序算法/插入排序.java
+++ 算法数据结构/src/main/java/排序算法/插入排序.java
@@ -1,9 +1,11 @@
 package 排序算法;
-
+/*
+ *  稳定  最好：n  平均：n**2 最坏：n**2
+ * */
 public class 插入排序 {
     public static void main(String[] args) {
         int[] a = {3, 4, 2, 1, -7, 0};
-        InsertMoveSort(a);
+        InsertTest(a);
         for (int i : a) {
             System.out.print(i + "\t");
         }
@@ -34,19 +36,19 @@ public class 插入排序 {
         int j;
         //第一种方法
         //{3, 4, 2, 1, -7, 0}
-        for (i = 1; i < arr.length; i++) {
-            int tmp = arr[i];
-            for (j = i; j >= 0; j--) {
-                if (arr[j - 1] > tmp) {
-                    arr[j] = arr[j - 1];
-                } else {
-                    break;
-                }
-            }
-            arr[j] = tmp;
-        }
-        // 第二种方法
-        for (int m = 1; i < arr.length; i++) {
+//        for (i = 1; i < arr.length-1; i++) {
+//            int tmp = arr[i];
+//            for (j = i; j >= 0; j--) {
+//                if (arr[j - 1] > tmp) {
+//                    arr[j] = arr[j - 1];
+//                } else {
+//                    break;
+//                }
+//            }
+//            arr[j] = tmp;
+//        }
+        // 第二种方法  //{3, 4, 2, 1, -7, 0}
+        for (int m = 1; m < arr.length; m++) {
             int tar = arr[m];
             int count = m - 1;
             while (count >= 0 && arr[count] > tar) {
@@ -57,6 +59,20 @@ public class 插入排序 {
         }
         return arr;
     }
+
+public static int[]  InsertTest(int[] arr){
+        for(int j=1;j<arr.length;j++){
+            int tar=arr[j];
+            int count=j-1;
+            while(count>=0 && arr[count]>tar){
+                arr[count+1]=arr[count];
+                count--;
+            }
+            arr[count+1] =tar;
+        }
+
+        return arr;
+}
 }
 
 
