1b47867db3c3722daa8e9c4d0f80ba7f0a3b6136
scma <59968873@qq.com>
2021-02-23 22:29:17 +0800

    默写冒泡排序

--- 算法数据结构/src/main/java/排序算法/冒泡排序.java
+++ 算法数据结构/src/main/java/排序算法/冒泡排序.java
@@ -1,11 +1,13 @@
 package 排序算法;
-
+/*
+* 稳定  最好：n  平均：n**2 最坏：n**2
+* */
 public class 冒泡排序 {
     public static void main(String[] args) {
         int[] a ={3,4,2,1,-7,0};
-        BubbleSort(a);
+//        BubbleSort(a);
 //        maopao2(a);
-        print(  BubbleSort(a));
+        print(  BubbleSortTest(a));
 
     }
 
@@ -31,5 +33,26 @@ public class 冒泡排序 {
             }
         }
         return  arr;
+    }
+
+
+
+    public static int[]  BubbleSortTest(int[] arr){
+        int flag=0;
+        for(int i=0;i<arr.length-1;i++){
+            for(int j=0;j<arr.length-1-i;j++){
+                if(arr[j]>arr[j+1]){
+                    int tem=arr[j+1];
+                    arr[j+1]=arr[j];
+                    arr[j]=tem;
+                    flag++;
+                }
+            }
+            if(flag==0){
+                break;
+            }
+
+        }
+        return arr;
     }
 }
